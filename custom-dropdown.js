a1e4cb615825bf342be9099a5201d74eeedc3c36
ekramer <ekramer@brownsteingroup.com>
2015-10-27 17:32:36 -0400

    started js custom dropdown

--- custom-dropdown.js
+++ custom-dropdown.js
@@ -1,4 +1,39 @@
 (function($){
 
-  
+  var items = $('select[data-dropdown-container]'),
+      containers = {},
+      htmlOpts = [];
+
+  $.each(items, function(index, item){
+
+    var $currentItem = $(item);
+
+    containers["dropdown-" + $currentItem.attr("name")] = $currentItem.data("dropdown-container");
+  });
+
+  $.each(containers, function(index, item){
+
+    $("<div class='dropdown " + index + "' data-selected data-state='closed'><div class='control'></div><div class='opts'></div>").appendTo(item);
+
+  });
+
+  $.each(items, function(index, item){
+
+    var $item = $(item),
+        options = $item.children(),
+        html = "";
+
+        html += "<ul>";
+        $.each(options, function(i, opt){
+
+          var $opt = $(opt);
+          console.log($opt);
+          html += "<li data-name='" + $opt.text().toLowerCase().replace(" ", "") + "'>" + $opt.text() + "</li>";
+
+        });
+        html += "</ul>";
+
+        htmlOpts.push(html);
+  });
+
 }(jQuery));
