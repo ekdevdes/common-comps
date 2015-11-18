364ff14186a9c3b7ab0f1424fa8c24d30713d78c
Ethan Kramer <ethankr@comcast.net>
2015-11-17 19:42:47 -0500

    dropdown now works like a dropdown and sets selected attribute for hidden dropdown

--- custom-dropdown.js
+++ custom-dropdown.js
@@ -1,9 +1,11 @@
 (function($){
 
+  // gathering the select's to transform
   var items = $('select[data-dropdown-container]'),
       containers = {},
       htmlOpts = {};
 
+  // building selects
   $.each(items, function(index, item){
 
     var $currentItem = $(item);
@@ -56,6 +58,7 @@
         $control = $dropdown.find(".control"),
         $opts = $dropdown.find(".opts");
 
+    // show/hide the options based on their current visibility status
     if($dropdown.attr("data-state") == "closed") {
       $opts.fadeIn("fast");
 
@@ -73,15 +76,36 @@
     e.preventDefault();
 
     var $dropdown = $(this).parent().parent().parent(),
-        $opts = $dropdown.find(".opts");
+        $opts = $dropdown.find(".opts"),
+        $selection = $(this),
+        origSelectName = $dropdown.attr("class").split(" ")[1].split("-")[1],
+        $origSelect = $('select[name="' + origSelectName + '"]'),
+        selectionText = $selection.html().replace(/(<([^>]+)>)/ig,"");
 
-    // capture clicked on element in a variable
+    // give a class to the selected element to identify it as selected so we can style it like so
+    $selection.addClass("selected");
 
+    // hide the options
     $opts.fadeOut("fast");
     $dropdown.attr("data-state", "closed");
 
     // set value of hidden select to selected item
+    $.each($origSelect.children(), function(index, option){
+
+      var $option = $(option),
+          optionText = $option.html();
+
+      if(optionText == selectionText) {
+        $option.attr("selected", "selected");
+      }
+    });
+
+    // set the data-selected attribute of the custom dropdown to the data-name attribute of the selected
+    // option for retaining of that same selection on page load
+    $dropdown.attr("data-selected", $selection.attr("data-name"));
+
     // set the shown value of the dropdown to the selected element
+    $dropdown.find(".control span").text(selectionText);
 
     return false;
   });
